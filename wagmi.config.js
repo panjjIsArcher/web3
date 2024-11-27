d2b334048eabc719139a32aa8f2a801c77191e1d
panjiajie <panjiajie@chexiao.co>
2024-11-27 16:41:52 +0800

    config

--- wagmi.config.js
+++ wagmi.config.js
@@ -1,8 +1,19 @@
-// @ts-check
+import { hardhat } from "@wagmi/cli/plugins";
+import { defineConfig, react } from "@wagmi/cli";
 
 /** @type {import('@wagmi/cli').Config} */
-export default {
-  out: 'src/generated.js',
-  contracts: [],
-  plugins: [],
-}
+export default defineConfig({
+  out: "src/generated.js",
+  contracts: [], // 合约列表
+  plugins: [
+    hardhat({
+      project: "./",
+      commands: {
+        clean: "pnpm hardhat clean",
+        build: "pnpm hardhat compile",
+        rebuild: "pnpm hardhat compile",
+      },
+    }),
+    react(), // 自动生成合约挂钩，简化前端代码
+  ],
+});
