b8474cea1e5f688f5bdefdff62ad70f62ac2ad82
panjiajie <panjiajie@chexiao.co>
2024-12-10 15:03:05 +0800

    按照ERC20声明solc

--- contracts/luxury/Seline.sol
+++ contracts/luxury/Seline.sol
@@ -2,5 +2,19 @@
 pragma solidity ^0.8.27;
 
 contract Seline {
-    constructor() {}
+    string public name;
+    string public symbol; // 代币符号或者缩写
+    uint8 public decimals; // 代币允许分割的小数位数
+    uint256 public totalSupply; // 代币总供应量
+    mapping(address => uint256) public balances; // 记录每个地址的余额
+
+    constructor() {
+        name = "Seline"; // 设置代币名称
+        symbol = "SL"; // 设置代币缩写
+        decimals = 18;
+        totalSupply = 1000000000 * (10 ** uint256(decimals)); // 设置代币总供应量
+    }
+
+    // 返回特定地址拥有的代币余额
+    function balanceOf(address _owner) public view returns (uint256) {}
 }
